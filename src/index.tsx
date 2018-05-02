0507781823c42f73ac03bb044b8a21476d1aa494
PathriK <pathrikumark@gmail.com>
2018-05-02 18:58:06 +0530

    Added Redux-devtools-extension

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension/developmentOnly';
 import App from './App';
 import './index.css';
 import bucksApp from './reducers';
 import registerServiceWorker from './utilities/registerServiceWorker';
 
-const store = createStore(bucksApp);
+const store = createStore(bucksApp, devToolsEnhancer({}));
 ReactDOM.render(
   <Provider store={store}>
   <App />
